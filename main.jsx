f9e3b80c6c644aec5cef04e76a9bb3abf486c5ee
Matheusalves112 <113462888+Matheusalves112@users.noreply.github.com>
2024-08-23 15:59:48 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,16 @@
+/* existing imports */
+import Contact from "./routes/contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
+]);
+
+/* existing code */
